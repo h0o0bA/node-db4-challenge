840467011cc01cf7bf43c47b523f4d930f08d047
Ihab Ali <ihab@live.com>
2019-08-15 06:56:50 -0500

    added getShoppingList(id) and getInstructions(id) access helpers

--- recipes/recipes-model.js
+++ recipes/recipes-model.js
@@ -2,9 +2,9 @@ const db = require("../data/dbConfig");
 
 module.exports = {
   getRecipes,
-  findById
-  //   getShoppingList,
-  //   getInstructions
+  findById,
+  getShoppingList,
+  getInstructions
   //   add,
   //   update,
   //   remove
@@ -27,3 +27,21 @@ function findById(id) {
       }
     });
 }
+
+function getShoppingList(id) {
+  return db("RecipesIngredients as ri")
+    .join("Recipes as r", "ri.RecipeID", "=", "r.id")
+    .join("Ingredients as i", "ri.IngredientID", "=", "i.id")
+    .where("RecipeID", id)
+    .select("r.RecipeName", "i.IngredientName", "ri.Quantity")
+    .orderBy("i.id");
+}
+
+function getInstructions(id) {
+  return db("recipes").where("id", id);
+}
+//   return db("steps as s")
+//     .join("recipes as r", "s.RecipeID", "r.id")
+//     .where("RecipeID", id)
+//     .select("r.RecipeName", "s.StepNumber", "s.instruction")
+//     .orderBy("StepNumber");
--- recipes/recipes-router.js
+++ recipes/recipes-router.js
@@ -37,4 +37,50 @@ router.get("/:id", async (req, res) => {
   }
 });
 
+// GET A INGREDIENT OF A RECIPE BY RECIPE ID
+router.get("/:id/shoppingList", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const IngredientsOfRecipe = await Recipes.getShoppingList(id);
+
+    if (IngredientsOfRecipe.length) {
+      res.json(IngredientsOfRecipe);
+    } else {
+      res.status(404).json({
+        message: `The recipe with the id: ${id} does not contain any ingredients so far.`
+      });
+    }
+  } catch (err) {
+    const { id } = req.params;
+
+    res.status(500).json({
+      error: `There was an error getting ingredients of the recipe with the id: ${id}.`
+    });
+  }
+});
+
+// GET A STEP OF A RECIPE BY RECIPE ID
+router.get("/:id/instructions", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const instructions = await Recipes.getInstructions(id);
+
+    if (instructions.length) {
+      res.json(instructions);
+    } else {
+      res.status(404).json({
+        message: `The recipe with the id: ${id} does not contain any instructions so far.`
+      });
+    }
+  } catch (error) {
+    const { id } = req.params;
+
+    res.status(500).json({
+      error:
+        `There was an error getting instructions of the recipe with the id: ${id}.` +
+        error
+    });
+  }
+});
+
 module.exports = router;
